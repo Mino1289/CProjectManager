613ef5029970e6c585c370fb5a3e704c289968ad
Mino1289 <kretzvictor@orange.fr>
2022-07-20 16:33:26 +0200

    Memory optimisation

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main(int argc, char *argv[]) {
         printf("Usage: %s <project_name> <..libnames>\n", argv[0]);
         return 1;
     }
-    char* project_name = (char*) malloc(strlen(argv[1]) + 1);
-    memcpy(project_name, argv[1], strlen(argv[1]) + 1);
+    char* project_name = (char*) malloc(sizeof(char) * (strlen(argv[1]) + 1));
+    memcpy(project_name, argv[1], sizeof(char) * (strlen(argv[1]) + 1));
     printf("Creating project \"%s\"\n", project_name);
 #ifdef _WIN32
     mkdir(project_name);
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
 
     char** libs = (char**) malloc(sizeof(char*));
     for (int i = 1; i < argc; i++) {
-        libs[i-1] = (char*) malloc(sizeof(char)*strlen(argv[i]));
+        libs[i-1] = (char*) malloc(sizeof(char) * strlen(argv[i]));
         strcpy(libs[i-1], argv[i]);
 
         printf("Creating %s.c && %s.h\n", libs[i-1], libs[i-1]);
@@ -31,6 +31,12 @@ int main(int argc, char *argv[]) {
         libs = realloc(libs, sizeof(char*)*(i+1));
     }
     fill_makefile(cwd, project_name, libs, argc-1);
+    
+    for (int i = 1; i < argc; i++) {
+        free(libs[i]);
+    }
+    free(libs);
+    free(project_name);
 
     printf("Done !\nYou can start coding !\n");
     return 0;
